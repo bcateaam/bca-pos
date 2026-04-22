e001b676f65f889a6e16ab659b4be7eb3936b7d3
bcateaam <i9oxoh@gmail.com>
2026-04-23 00:47:25 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BCA POS',
+      theme: ThemeData.dark(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('BCA POS')),
+      body: const Center(
+        child: Text(
+          'Welcome to BCA POS',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
